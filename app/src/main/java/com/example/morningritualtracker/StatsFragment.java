2e5efbc1c61fd575abb519b27ae908fae15b0203
marko-kreso <markokreso@gmail.com>
2019-11-30 17:08:06 -0700

    reads data and links calendar to file by date correctly

--- app/src/main/java/com/example/morningritualtracker/StatsFragment.java
+++ app/src/main/java/com/example/morningritualtracker/StatsFragment.java
@@ -11,10 +11,18 @@ import android.widget.DatePicker;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TreeMap;
+
 public class StatsFragment extends Fragment {
 
     private Activity containerActivity = null;
     private View inflaterView;
+    private TreeMap<Date, CompletedDay> graphData;
 
     public void setContainerActivity(Activity containerActivity) {
         this.containerActivity = containerActivity;
@@ -32,7 +40,7 @@ public class StatsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         DatePicker simpleDatePicker = (DatePicker)containerActivity.findViewById(R.id.calendar);
-
+        loadGraphData();
         // TODO: this takes the text but breaks the UI
         // TODO 2: have the Calendar take less space, maybe a different type
         /*simpleDatePicker.getCalendarView().setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
@@ -53,7 +61,22 @@ public class StatsFragment extends Fragment {
     }
 
     public void openDay(){
+
+        //Grabs date from calendar to yyyyMMdd format String
+        DatePicker selectDate = (DatePicker) inflaterView.findViewById(R.id.calendar);
+        String year = Integer.toString(selectDate.getYear());
+        String month = Integer.toString(selectDate.getMonth()+1);
+        String day = Integer.toString(selectDate.getDayOfMonth());
+        String dateSelected = year+month+day;
+
+        //Passes data on to new fragment
+        Bundle args = new Bundle();
+        args.putSerializable("day", getDate(dateSelected));
+        args.putSerializable("date", dateSelected);
+
+
         DayFragment fragment = new DayFragment();
+        fragment.setArguments(args);
         fragment.setContainerActivity(containerActivity);
         FragmentTransaction transaction = getFragmentManager().beginTransaction();
         transaction.replace(R.id.outer, fragment);
@@ -69,5 +92,34 @@ public class StatsFragment extends Fragment {
                 openDay();
             }
         });
+    }
+
+    public CompletedDay getDate(String dateSelected){
+        System.out.println("DATE: " + dateSelected);
+        CompletedDay retrievedDate = null;
+        try {
+             retrievedDate = graphData.get(new SimpleDateFormat("yyyyMMdd").parse(dateSelected));
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return retrievedDate;
+    }
+
+    public void loadGraphData(){
+        File root = new File(containerActivity.getExternalFilesDir(null) + "/days");
+        graphData = new TreeMap<Date, CompletedDay>();
+        FileInputStream fileIn;
+        ObjectInputStream objectIn;
+        try {
+            for (File file : root.listFiles()) {
+                fileIn = new FileInputStream(file);
+                objectIn = new ObjectInputStream(fileIn);
+                CompletedDay day = (CompletedDay) objectIn.readObject();
+                Date date = new SimpleDateFormat("yyyyMMdd").parse(file.getName());
+                graphData.put(date, day);
+            }
+        }
+        catch(Exception e){e.printStackTrace();}
     }
 }
